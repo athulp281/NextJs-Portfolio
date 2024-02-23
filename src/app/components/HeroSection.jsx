ee470d12d421aa2052c98751df8ef21576b51732
athul <athulpranoor@gmail.com>
2024-02-23 23:45:26 +0530

    nav bar

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 function HeroSection() {
     return (
@@ -10,7 +12,27 @@ function HeroSection() {
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                             Hello, I am{""}
                         </span>
-                        Athul
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                "Athul",
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                "Developer",
+                                1000,
+                                "Designer",
+                                1000,
+                                "Photographer",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            style={{
+                                fontSize: "1.4em",
+                                display: "inline-block",
+                            }}
+                            repeat={Infinity}
+                        />
                     </h1>
                     <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                         Lorem, ipsum dolor sit amet consectetur adipisicing
@@ -23,8 +45,10 @@ function HeroSection() {
                         <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
                             Hire Me
                         </button>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
-                            Download CV
+                        <button className="px-6 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple to-pink-500 hover:bg-slate-800 text-white  mt-3">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                Download CV
+                            </span>
                         </button>
                     </div>
                 </div>
