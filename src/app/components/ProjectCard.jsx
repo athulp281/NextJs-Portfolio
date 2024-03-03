20d481b211c3f580d8f90a05ed0a1d05493263ac
athul <athulpranoor@gmail.com>
2024-03-03 23:14:40 +0530

    card update

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -4,12 +4,18 @@ const ProjectCard = ({ imgUrl, title, description }) => {
     return (
         <div>
             <div
-                className="h-52 md:h-72"
+                className="h-52 md:h-72 rounded-t-xl relative group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 "
                 style={{
                     background: `url(${imgUrl})`,
                     backgroundSize: "cover",
                 }}
-            ></div>
+            >
+                <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden "></div>
+            </div>
+            <div className="text-white rounded-b-xl bg-[#181818]py-6 px-4">
+                <h5 className="text-xl font-semibold mb-2">{title}</h5>
+                <p className="text-[#ABD7BE]">{description}</p>
+            </div>
         </div>
     );
 };
--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projects = [
+    {
+        id: 1,
+        title: "Sample Title 1",
+        description: "Description for sample item 1",
+        image: "/images/projects/1.jpg",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 2,
+        title: "Sample Title 2",
+        description: "Description for sample item 2",
+        image: "/images/projects/2.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 3,
+        title: "Sample Title 3",
+        description: "Description for sample item 3",
+        image: "/images/projects/3.jpg",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 4,
+        title: "Sample Title 3",
+        description: "Description for sample item 3",
+        image: "/images/projects/4.jpg",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 5,
+        title: "Sample Title 3",
+        description: "Description for sample item 3",
+        image: "/images/projects/5.jpg",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 6,
+        title: "Sample Title 3",
+        description: "Description for sample item 3",
+        image: "/images/projects/6.jpg",
+        tag: ["All", "Web"],
+    },
+    // Add more objects as needed
+];
 
 const ProjectSection = () => {
     return (
         <>
             <h2>My Projects</h2>
+            <div>
+                {projects.map((item) => {
+                    return (
+                        <ProjectCard
+                            key={item.id}
+                            title={item.title}
+                            description={item.description}
+                            imgUrl={item.image}
+                        />
+                    );
+                })}
+            </div>
         </>
     );
 };
