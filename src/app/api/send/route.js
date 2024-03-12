709d6f58b29c699a48f4c5f8361bc467f763adfe
athul <athulpranoor@gmail.com>
2024-03-12 16:31:29 +0530

    update

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,28 +1,28 @@
 // import { EmailTemplate } from "../../../components/EmailTemplate";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-    const { body } = req;
-    const { email, subject, message } = body;
-    try {
-        const data = await resend.emails.send({
-            from: fromEmail,
-            to: `Athul,<athul.interval@gmail.com>`,
-            subject: "hello",
-            react: (
-                <>
-                    <h1>Hello</h1>
-                    <p>Thank you for contacting us!</p>
-                    <p>Hello</p>
-                </>
-            ),
-        });
+// export async function POST(req, res) {
+//     const { body } = req;
+//     const { email, subject, message } = body;
+//     try {
+//         const data = await resend.emails.send({
+//             from: fromEmail,
+//             to: `Athul,<athul.interval@gmail.com>`,
+//             subject: "hello",
+//             react: (
+//                 <>
+//                     <h1>Hello</h1>
+//                     <p>Thank you for contacting us!</p>
+//                     <p>Hello</p>
+//                 </>
+//             ),
+//         });
 
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error });
-    }
-}
+//         return Response.json(data);
+//     } catch (error) {
+//         return Response.json({ error });
+//     }
+// }
